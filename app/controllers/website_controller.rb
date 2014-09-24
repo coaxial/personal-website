bef29cbf00eb7c0358f16e56dbba2f5591b67670
Pierre-Yves Poujade <py@poujade.org>
2014-09-24 09:44:03 -0400

    Add time_greeting helper + tests

--- app/controllers/website_controller.rb
+++ app/controllers/website_controller.rb
@@ -1,4 +1,7 @@
 class WebsiteController < ApplicationController
+  include WebsiteHelper
+
   def index
+    @greeting = time_greeting
   end
 end
--- spec/controllers/website_controller_spec.rb
+++ spec/controllers/website_controller_spec.rb
@@ -1,12 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe WebsiteController, :type => :controller do
+  describe "#index" do
+    context "in the morning" do
+      before :example do
+        allow(Time).to receive(:now).and_return(Time.parse "13 Sep 2014 05:07")
+      end
 
-  describe "GET index" do
-    it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
+      it "returns 'good morning'" do
+        get :index
+        expect(assigns(:greeting)).to eq('good morning')
+      end
+    end
+
+    context "in the afternoon" do
+      before :example do
+        allow(Time).to receive(:now).and_return(Time.parse "13 Sep 2014 13:37")
+      end
+
+      it "returns 'good afternoon'" do
+        get :index
+        expect(assigns(:greeting)).to eq('good afternoon')
+      end
+    end
+
+    context "in the evening" do
+      before :example do
+        allow(Time).to receive(:now).and_return(Time.parse "13 Sep 2014 01:38")
+      end
+
+      it "returns 'good evening'" do
+        get :index
+        expect(assigns(:greeting)).to eq('good evening')
+      end
     end
   end
-
 end
