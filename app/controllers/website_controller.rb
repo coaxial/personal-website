1b961fab0f985e455fec0ff69107373c8589af53
Pierre-Yves Poujade <py@poujade.org>
2014-09-24 23:19:05 -0400

    Validate form, send email, add friendly error messages

--- app/controllers/website_controller.rb
+++ app/controllers/website_controller.rb
@@ -4,4 +4,18 @@ class WebsiteController < ApplicationController
   def index
     @greeting = time_greeting
   end
+
+  def writeme
+    @contact_form = ContactForm.new params[:writeme]
+
+    if @contact_form.valid?
+      FormMailer.send_visitor_message(@contact_form).deliver
+      flash[:notice] = "Message sent."
+      redirect_to action: :index
+    else
+      flash[:alert]  = "Message not sent."
+      flash[:errors] = @contact_form.errors.full_messages
+      redirect_to action: :index, params: params
+    end
+  end
 end
---
+++ spec/mailers/form_mailer_spec.rb
@@ -0,0 +1,8 @@
+require "rails_helper"
+
+RSpec.describe FormMailer, :type => :mailer do
+  it "sends an email to me" do
+    let(:senderAddr) { "visitor@example.com" }
+    
+  end
+end
