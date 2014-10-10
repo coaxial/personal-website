128d641c90972fa3ea66e89b86d8bc89fd45671b
Pierre-Yves Poujade <py@poujade.org>
2014-10-09 23:35:21 -0400

    Add Daftpunk video autoplay modal

--- app/assets/javascripts/website.js
+++ app/assets/javascripts/website.js
@@ -1,5 +1,22 @@
-setInterval(function() {
-  ageInSecs = Number($('#age_in_secs').text().replace(/,/g, '')) + 1
-  ageInSecs = ageInSecs.toString().replace(/([0-9]{3})/g, '$1,').replace(/,$/, '')
-  $('#age_in_secs').text(ageInSecs)
-}, 1000)
+$(window).bind("load", function() {
+
+  // Autoplay the YouTube video when opening the modal
+    $('[data-reveal-id="daftpunk-modal"]').click(function () {
+    var originalSrcValue = $('#daftpunk-modal iframe').attr('src')
+    console.log('open triggered')
+    $('#daftpunk-modal iframe').attr('src', originalSrcValue + '?autoplay=1')
+  })
+
+  // And stop playing when closing it
+  $(document).on('close.fndtn.reveal', '[data-reveal]', function() {
+    var originalSrcValue = $('#daftpunk-modal iframe').attr('src')
+    $('#daftpunk-modal iframe').attr('src', originalSrcValue.replace(/\?autoplay\=1/, ''))
+  })
+
+  // Update the seconds count every second
+  setInterval(function() {
+    var ageInSecs = Number($('#age_in_secs').text().replace(/,/g, '')) + 1
+    ageInSecs = ageInSecs.toString().replace(/([0-9]{3})/g, '$1,').replace(/,$/, '')
+    $('#age_in_secs').text(ageInSecs)
+  }, 1000)
+})
