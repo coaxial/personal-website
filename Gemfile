04621ab8cde18a805b37020db27d0ad73928a37e
Pierre-Yves Poujade <py@poujade.org>
2015-06-08 21:04:54 -0400

    Fix broken test

--- Gemfile
+++ Gemfile
@@ -19,10 +19,10 @@ gem 'jquery-rails'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
-gem 'sdoc', '~> 0.4.0',          group: :doc
+gem 'sdoc', '~> 0.4.0', group: :doc
 
 # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
-gem 'spring',        group: :development
+gem 'spring', group: :development
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
@@ -70,6 +70,9 @@ gem 'rails_12factor'
 
 # Code climate test coverage
 gem "codeclimate-test-reporter", group: :test, require: nil
+
+# Freeze time
+gem 'timecop'
 
 # Use Puma instead of WEBrick
 gem 'puma'
--- spec/controllers/website_controller_spec.rb
+++ spec/controllers/website_controller_spec.rb
@@ -2,9 +2,13 @@ require 'rails_helper'
 
 RSpec.describe WebsiteController, :type => :controller do
   describe "#index" do
+    after :each do
+      Timecop.return
+    end
+
     context "in the morning" do
-      before :example do
-        allow(Time).to receive(:now).and_return(Time.parse "13 Sep 2014 05:07")
+      before do
+        Timecop.freeze(Time.new(2014, 9, 13, 5, 7, 0,'-04:00'))
       end
 
       it "returns 'good morning'" do
@@ -14,8 +18,8 @@ RSpec.describe WebsiteController, :type => :controller do
     end
 
     context "in the afternoon" do
-      before :example do
-        allow(Time).to receive(:now).and_return(Time.parse "13 Sep 2014 13:37")
+      before do
+        Timecop.freeze(Time.new(2014, 9, 13, 13, 37, 0,'-04:00'))
       end
 
       it "returns 'good afternoon'" do
@@ -25,8 +29,8 @@ RSpec.describe WebsiteController, :type => :controller do
     end
 
     context "in the evening" do
-      before :example do
-        allow(Time).to receive(:now).and_return(Time.parse "13 Sep 2014 01:38")
+      before do
+        Timecop.freeze(Time.new(2014, 9, 13, 1, 37, 0,'-04:00'))
       end
 
       it "returns 'good evening'" do
